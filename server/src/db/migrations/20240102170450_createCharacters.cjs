cfc06fee3ea8081ab440b704d765896fed2214d7
RipNasty <det625@gmail.com>
2024-01-02 12:30:57 -0500

    model made

--- server/src/db/migrations/20240102170450_createCharacters.cjs
+++ server/src/db/migrations/20240102170450_createCharacters.cjs
@@ -12,6 +12,9 @@ exports.up = async (knex) => {
         table.string("class")
         table.integer("level")
         table.integer("experience")
+        table.integer("stepCount")
+        table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
+        table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
     })
 }
 
