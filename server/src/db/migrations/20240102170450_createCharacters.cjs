e2a47f4bd7b148a7e68f2ece8b18dbc4c78c6fee
RipNasty <det625@gmail.com>
2024-01-02 12:10:27 -0500

    2

--- server/src/db/migrations/20240102170450_createCharacters.cjs
+++ server/src/db/migrations/20240102170450_createCharacters.cjs
@@ -6,7 +6,13 @@
  * @param {Knex} knex
  */
 exports.up = async (knex) => {
-    return knex.schema.createTable("characters", (table))
+    return knex.schema.createTable("characters", (table) => {
+        table.bigIncrements("id")
+        table.string("name")
+        table.string("class")
+        table.integer("level")
+        table.integer("experience")
+    })
 }
 
 /**
