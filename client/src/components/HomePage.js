422c5f9995bf9574ac467540c15924dabf6a47c5
RipNasty <det625@gmail.com>
2024-04-28 12:13:00 -0400

    3

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 const HomePage = (props) => {
-    
+
+
+    return (
+        <div>
+            <h2>Wayfarers</h2>
+            <h3>Microdosing Multiplayer Roguelike</h3>
+        </div>
+    )
 }
